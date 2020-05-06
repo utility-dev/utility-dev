01082d8e03008c79d18b36f3b0fcf1d4cb94f68a
utility-dev <64712384+utility-dev@users.noreply.github.com>
2020-05-06 19:09:13 +0200

    Update sendCode.js

--- js/sendCode.js
+++ js/sendCode.js
@@ -20,13 +20,10 @@ $(function() {
             }
             $.ajax({
                 url: "https://tsoumbou.pythonanywhere.com/api/checkregister/".concat(code),
-                type: "POST",
-                data: {
-                    token: code
-                },
+                type: "GET",
                 cache: false,
                 success: function(response) {
-                   var res = JSON.parse(res)
+                   var res = JSON.parse(response)
                    if(res.checkToken == "OK"){
                        // Success message
                       $('#success').html("<div class='alert alert-success'>");
@@ -56,7 +53,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + " it seems that my mail server is not responding...</strong> Could you please email me directly to <a href='mailto:me@example.com?Subject=Message_Me from myprogrammingblog.com;>me@example.com</a> ? Sorry for the inconvenience!");
+                    $('#success > .alert-danger').append("<strong>Sorry "  + " it seems that my mail server is not responding...</strong> Could you please email me directly to <a href='mailto:me@example.com?Subject=Message_Me from myprogrammingblog.com;>me@example.com</a> ? Sorry for the inconvenience!");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
