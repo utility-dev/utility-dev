ad820fbadd9734d52c0b6e7f4e45a1efd94e0221
utility-dev <64712384+utility-dev@users.noreply.github.com>
2020-05-06 18:40:19 +0200

    Update sendCode.js

--- js/sendCode.js
+++ js/sendCode.js
@@ -25,18 +25,31 @@ $(function() {
                     token: code
                 },
                 cache: false,
-                success: function() {
-                    // Success message
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
-                    $('#success > .alert-success')
-                        .append('</div>');
+                success: function(response) {
+                   var res = JSON.parse(res)
+                   if(res.checkToken == "OK"){
+                       // Success message
+                      $('#success').html("<div class='alert alert-success'>");
+                      $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                          .append("</button>");
+                      $('#success > .alert-success')
+                          .append("<strong>Your message has been sent. </strong>");
+                      $('#success > .alert-success')
+                          .append('</div>');
 
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
+                      //clear all fields
+                      $('#contactForm').trigger("reset");
+                   }else{
+                          // Fail message
+                        $('#success').html("<div class='alert alert-danger'>");
+                        $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                            .append("</button>");
+                        $('#success > .alert-danger').append("<strong>Sorry " + firstName + " it seems that my mail server is not responding...</strong> Could you please email me directly to <a href='mailto:me@example.com?Subject=Message_Me from myprogrammingblog.com;>me@example.com</a> ? Sorry for the inconvenience!");
+                        $('#success > .alert-danger').append('</div>');
+                        //clear all fields
+                        $('#contactForm').trigger("reset");
+                   }
+                    
                 },
                 error: function() {
                     // Fail message
