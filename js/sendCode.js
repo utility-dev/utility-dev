83304d5a885701a389711af5733b0a65dcc61d33
utility-dev <64712384+utility-dev@users.noreply.github.com>
2020-05-06 21:08:55 +0200

    Update sendCode.js

--- js/sendCode.js
+++ js/sendCode.js
@@ -22,10 +22,7 @@ $(function() {
                 url: "https://tsoumbou.pythonanywhere.com/api/checkregister/".concat(code),
                 dataType: "json",
                 cache: false,
-                success: function(data) {
-					
-		   
-                   if(JSON.parse(data).checkToken == "OK"){
+                success: function() {
                        // Success message
                       $('#success').html("<div class='alert alert-success'>");
                       $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
@@ -33,17 +30,7 @@ $(function() {
                       $('#success > .alert-success')
                           .append("<strong>Your message has been sent. </strong>");
                       $('#success > .alert-success')
-                          .append('</div>');
-				   }else{
-					       // Fail message
-                        $('#success').html("<div class='alert alert-danger'>");
-                        $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                            .append("</button>");
-                        $('#success > .alert-danger').append("<strong>Sorry "  + " it seems that my mail server is not responding...</strong> Could you please email me directly to <a href='mailto:me@example.com?Subject=Message_Me from myprogrammingblog.com;>me@example.com</a> ? Sorry for the inconvenience!");
-                        $('#success > .alert-danger').append('</div>');
-                        //clear all fields
-                        $('#contactForm').trigger("reset");
-				   }
+                          .append('</div>');		   
                 },
                 error: function() {
                     // Fail message
