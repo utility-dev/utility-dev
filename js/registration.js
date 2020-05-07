71bfd3d5a7a564e8fca269a3b036ff64db28ff06
utility-dev <64712384+utility-dev@users.noreply.github.com>
2020-05-07 09:57:53 +0200

    Add files via upload

---
+++ js/registration.js
@@ -0,0 +1,51 @@
+/*
+  Jquery Validation using jqBootstrapValidation
+   example is taken from jqBootstrapValidation docs 
+  */
+$(function() {
+
+    $("div.form-horizontal input").jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function($form, event, errors) {
+            // something to have when submit produces an error ?
+            // Not decided if I need it yet
+        },
+        submitSuccess: function($form, event) {
+            event.preventDefault(); // prevent default submit behaviour
+            // get values from FORM
+			var genre = $("#gender :selected").val()
+            var name = $("div.form-horizontal input#nom").val();
+            var tel = $("div.form-horizontal input#mobile").val();
+            var pass = $("div.form-horizontal input#password").val();
+            var firstName = $("div.form-horizontal input#prenom").val();
+            $.ajax({
+                url: "https://tsoumbou.pythonanywhere.com/api/save/".concat(name,"/",firstname,"/",tel,"/",gender,"/",pass),
+                type: "json",
+                cache: false,
+                success: function() {
+                    // Success message
+                    $('#Registration').hide();
+					$('#sendCodeForm').show();
+                },
+                error: function() {
+                    $('#succes').html("<h3 class='other-nw alert-danger'>Erreur lors de l'inscription</h3>");
+                    //clear all fields
+                },
+            })
+        },
+        filter: function() {
+            return $(this).is(":visible");
+        },
+    });
+
+    $("a[data-toggle=\"tab\"]").click(function(e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
+
+
+/*When clicking on Full hide fail/success boxes */
+$('#name').focus(function() {
+    $('#success').html('');
+});
