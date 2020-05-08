250f2a4e51adac6fa92179910069bb25c6e7a56e
utility-dev <64712384+utility-dev@users.noreply.github.com>
2020-05-08 11:34:20 +0200

    Update connexion.js

--- js/connexion.js
+++ js/connexion.js
@@ -13,49 +13,22 @@ $(function() {
         submitSuccess: function($form, event) {
             event.preventDefault(); // prevent default submit behaviour
             // get values from FORM
-            var tel = $("div.w3ls-connexionForm input#tel").val();
+	        var tel = $("div.w3ls-connexionForm input#tel").val();
             var pass = $("div.w3ls-connexionForm input#exampleInputPassword1").val();
-			
-			var request = new XMLHttpRequest();
-
-			request.open('GET', "https://tsoumbou.pythonanywhere.com/api/auth/".concat(tel,"/",pass), true)
-			request.onload = function() {
-			  var data = JSON.parse(this.response);
-      		          console.log(data);
-			  	
-
-			  if (request.status >= 200 && request.status < 400) {
-				sessionStorage.setItem("firstname", data.firstname);
-				sessionStorage.setItem("name", data.name);
-				sessionStorage.setItem("phone", data.phone);
-				sessionStorage.setItem("gender", data.gender);
-			     $(location).attr('href','https://utility-dev.github.io/index.html');
-			     $('#succesConnexion').empty();
-			  } else {
-				console.log('error');
-			  }
-			}
-
-			request.send();
-            /*$.ajax({
+            $.ajax({
                 url: "https://tsoumbou.pythonanywhere.com/api/auth/".concat(tel,"/",pass),
                 dataType: "json",
                 cache: false,
-                success: function(data) {
+                success: function() {
                     // Success message
-					var obj = JSON.parse(data);
-					sessionStorage.setItem("firstname", obj.firstname);
-					sessionStorage.setItem("name", obj.name);
-					sessionStorage.setItem("phone", obj.phone);
-					sessionStorage.setItem("gender", obj.gender);
-					$(location).attr('href','https://utility-dev.github.io/index.html');
-					$('#succesConnexion').empty();
+                    $('#login').hide();
+		            $('#sendCodeConxForm').show();
                 },
                 error: function() {
                     $('#succesConnexion').html("<h3 class='other-nw alert-danger'>Numéro ou mot de passe incorrect</h3>");
                     //clear all fields
                 },
-            })*/
+            })
         },
         filter: function() {
             return $(this).is(":visible");
@@ -69,9 +42,7 @@ $(function() {
 });
 
 
-/*When clicking on deconnexion button */
-$('#deconnexion').click(function() {
-	alert("test");
-    //sessionStorage.clear();
-   // $(location).attr('href','https://utility-dev.github.io/index.html');
+/*When clicking on Full hide fail/success boxes */
+$('#name').focus(function() {
+    $('#success').html('');
 });
