9bfcf1fc33a5a40198464baa38b9de22a8b096b0
JW5943 <terrencesoumboulepinda@yahoo.fr>
2020-08-08 12:19:22 +0200

    fix previous commit

--- js/requestBillet.js
+++ js/requestBillet.js
@@ -7,7 +7,7 @@
 function testa() {
 
             var villeDepart = $("div.w3ls-requestAVForm input#planeInputDepart").val();
-			var VilleArrive = $("div.w3ls-requestAVForm input#planeInputArrive").val();
+			var villeArrive = $("div.w3ls-requestAVForm input#planeInputArrive").val();
 			var datepickerDepart = $("div.w3ls-requestAVForm input#datepicker").val();
 			var datepickerArrive = $("div.w3ls-requestAVForm input#datepicker1").val();
 			var departAsTab = datepickerDepart.split("/");
@@ -16,48 +16,88 @@ function testa() {
 			var arrive = "".concat(arriveAsTab[2],"-", arriveAsTab[0], "-",  arriveAsTab[1]);
 			var type = "Avion";
             // Check for white space in code for Success/Fail message
-            var url= "https://tsoumbou.pythonanywhere.com/api/request/".concat(villeDepart,"/",VilleArrive,"/",depart,"/",arrive,"/",type);
-         window.indexedDB = window.indexedDB || window.mozIndexedDB || 
-         window.webkitIndexedDB || window.msIndexedDB;
-         
-         //prefixes of window.IDB objects
-         window.IDBTransaction = window.IDBTransaction || 
-         window.webkitIDBTransaction || window.msIDBTransaction;
-         window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || 
-         window.msIDBKeyRange
-         
-         if (!window.indexedDB) {
-            window.alert("Your browser doesn't support a stable version of IndexedDB.")
-         }
-         
-         var db;
-         var request = window.indexedDB.open("utility-db", 1);
-         
-         request.onerror = function(event) {
-            console.log("error: ");
-         };
-         
-         request.onsuccess = function(event) {
-            db = request.result;
-            console.log("success: "+ db);
-			var objectStore = db.transaction(["requestBillet"], "readwrite").objectStore("requestBillet");
-			var transaction = db.transaction(["requestBillet"], "readwrite");
-			var objectStore = transaction.objectStore("requestBillet");
-			var objectStoreRequest = objectStore.clear();
-			objectStore.add({ id: "00-01", url: url, time: new Date().getTime()});
-			console.log(data)
+            var url= "https://tsoumbou.pythonanywhere.com/api/request/".concat(villeDepart,"/",villeArrive,"/",depart,"/",arrive,"/",type);
+			sessionStorage.setItem('depart', villeDepart);
+			sessionStorage.setItem('arrive', villeArrive);
+			sessionStorage.setItem('requestBillet', url);
+			
+			window.location.href = "https://utility-dev.github.io/vols-list.html";
             
-         };
-         
-         request.onupgradeneeded = function(event) {
-            var db = event.target.result;
-            var objectStore = db.createObjectStore("requestBillet", {keyPath: "id"});
-			for (var i in data) {
-               objectStore.add(data[i]);
-            }
-                        
-         } 
+         }; 
 }
+
+function testa1() {
+    var url = sessionStorage.getItem('requestBillet');
+	var data = getDataFromUrl(url);
+	
+    output = document.getElementById('output')
+    output.innerHTML = json2Table(data)
+}
+
+
+function getDataFromUrl(url){
+	var data;
+	$.ajax({
+                url: url,
+                dataType: "json",
+                cache: false,
+                success: function(d) {
+                       // Success message
+                    data = d;
+                },
+                error: function() {
+                    // Fail message
+                    console.log("cannot retrieve data from url :".concat(url));
+                    
+                },
+            })
+	return data;
+}
+
+function json2Table(json) {
+  let cols = ["No. du vol", "Nom de la compagnie", "Heure de Depart", "Heure d'Arrivée", "Jours de circulation", "prix"];
+
+  let headerRow = cols
+    .map(col => '<th>${col}</th>')
+    .join("");
+
+  let rows = json
+    .map(row => {
+      let tds = cols.map(col => '<td>${row[col]}</td>').join("");
+      return '<tr>${tds}</tr>';
+    })
+    .join("");
+
+    let rows = ""
+	$.map(json, function(val, key) {
+	  if(key.localeCompare("REFERENCE")==0){
+		  res.join('<td class="t-one">').join(val).join("</td>");
+		  res.join('<td class="wthree"><i class="fa fa-plane" aria-hidden="true"></i> Train name </td>');
+	  }else if(key.localeCompare("DEPART")==0){
+		  res.join('<td class="wthree"><i class="fa fa-clock-o"></i>').join(val).join('</td>');
+	  }else if(key.localeCompare("ARRIVE")==0){
+		  res.join('<td class="wthree"><i class="fa fa-clock-o"></i>').join(val).join('</td>');
+	  }else if(key.localeCompare("PRICE")==0){
+		  res.join('<td class="price us">').join(val).join('<a href="#" data-toggle="modal" data-target="#myModalbook" class="seat-button two">Book Now</a></td>');
+	  }
+	  
+
+	});
+
+  //build the table
+  const table = `
+	<table class="table table-bordered agileinfo"> 
+		<thead>
+			<tr>${headerRow}</tr>
+		<thead>
+		<tbody>
+			${rows}
+		<tbody>
+	<table>`;
+
+  return table;
+}
+
 
 
 var countries = [
